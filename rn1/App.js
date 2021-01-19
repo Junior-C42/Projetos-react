131b2971082c6aa229080fdbbf66c28f1920895c
Junior Chagas <jjuniorchagas15@gmail.com>
2021-01-19 02:32:20 -0300

    Adicionando arquivo da video-aula sobre StatusBar

--- rn1/App.js
+++ rn1/App.js
@@ -1,36 +1,14 @@
 // import-area:
-import React, { useEffect, useState } from 'react'; 
-import { StatusBar } from 'react-native'; 
+import React from 'react'; 
+import { View } from 'react-native'; 
 import styled from 'styled-components/native'; 
 
 // render-area:
 export default () => {
-  
-  const [ hidden, setHidden ] = useState(false); 
-  useEffect(()=>{
-    
-  }, [])
   return(
-    <Page>
-      <Botao
-        title="Toggle StatusBar"
-        onPress={ ()=>{setHidden(!hidden)} }
-      />
-      <StatusBar 
-        barStyle='higth-content' 
-        backgroundColor='#0000ff'
-        hidden={hidden}
-      />
-    </Page>
+    <View>
+
+    </View>
   ); 
 }; 
-
-// styled-ara:
-const Page = styled.View`
-  flex:1; 
-  justify-content:center; 
-  align-items:center;
-  background-color:#0000ff; 
-`; 
-const Botao = styled.Button`
-`; 
+// styled-area:
---
+++ rn1/src/components/StatusBar.js
@@ -0,0 +1,37 @@
+// import-area:
+import React, { useEffect, useState } from 'react'; 
+import { StatusBar } from 'react-native'; 
+import styled from 'styled-components/native'; 
+
+// render-area:
+export default () => {
+  
+  const [ hidden, setHidden ] = useState(false); 
+  useEffect(()=>{
+    
+  }, [])
+  return(
+    <Page>
+      <Botao
+        title="Toggle StatusBar"
+        onPress={ ()=>{setHidden(!hidden)} }
+      />
+      <StatusBar 
+        barStyle='higth-content' 
+        backgroundColor='#0000ff'
+        hidden={hidden}
+        animated={true}
+      />
+    </Page>
+  ); 
+}; 
+
+// styled-ara:
+const Page = styled.View`
+  flex:1; 
+  justify-content:center; 
+  align-items:center;
+  background-color:#0000ff; 
+`; 
+const Botao = styled.Button`
+`; 
