673c77d7ba3641c35dca564adaab21cd3c35f1d6
Junior Chagas <jjuniorchagas15@gmail.com>
2021-01-19 07:55:22 -0300

    Criando app do modulo sobre ReactNavigation4.0 da b7web

---
+++ ModuloReactNavigation/App.js
@@ -0,0 +1,18 @@
+// import-area:
+import React, { useState } from 'react'; 
+import { View, StyleSheet } from 'react-native'; 
+
+// render-area:
+export default () => {
+  return(
+    <View style={styles.container}>
+
+    </View>
+  ); 
+}; 
+// styled-area:
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  }, 
+})
